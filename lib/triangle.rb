bf7dd4864c6d50c2a1832c8e7a87a4799551b095
Brian Craycraft <craycraftb24@hotmail.com>
2020-06-19 02:32:30 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -8,6 +8,19 @@ class Triangle
   end
   
   def kind
+    if (@side_a <= 0) || (@side_b <= 0) || (@side_c <= 0)
+      raise TriangleError
+    elsif (@side_a+@side_b <= @side_c) || (@side_a+@side_c <= @side_b) || (@side_b+@side_c <= @side_a)
+      raise TriangleError
+    else
+      if (@side_a == @side_b) && (@side_b == @side_c)
+        :equilateral
+      elsif (@side_a == @side_b) || (@side_b == @side_c) || (@side_a == @side_c)
+        :isosceles
+      elsif (@side_a != @side_b) && (@side_b != @side_c) && (@side_a != @side_c)
+        :scalene
+      end
+    end
     
   end
   
